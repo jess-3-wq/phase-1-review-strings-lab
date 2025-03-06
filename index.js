d396ed93be6480c5001294da5520ef37d89b338e
Jesse <jesse.yegon@student.moringaschool.com>
2025-03-06 08:50:10 -0500

    adds new string

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Jesse Yegon';
+
+const welcomeMessage = "Welcome to Flatbook, " + "Jesse Yegon !";
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + "JESSE YEGON!";
+
+const shortGreeting = "Welcome, " + "J !";
